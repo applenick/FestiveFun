54fb39652a4cc960a501a78e28fe8da11e3d9489
applenick <applenick3@me.com>
2014-07-06 21:37:26 -0700

    Fix format for DateTimeFormatter

--- src/main/java/com/applenick/FestiveFun/holiday/FestiveHolidays.java
+++ src/main/java/com/applenick/FestiveFun/holiday/FestiveHolidays.java
@@ -14,12 +14,12 @@ import de.jollyday.HolidayManager;
 
 public class FestiveHolidays {
 	
-	public static DateTimeFormatter dtf = DateTimeFormat.forPattern("E, MMMM, ee, yyyy");
+	public static DateTimeFormatter dtf = DateTimeFormat.forPattern("EEEE, MMMM, eeee, yyyy");
 	
 	public static void loginCheck(){		
 		HolidayManager hm = HolidayManager.getInstance(HolidayCalendar.UNITED_STATES);
-		Instant now = Instant.now();
 		Date date = new Date();
+		Instant now = Instant.now();
 		
 		Alog.consoleMsg(ChatColor.GOLD + "Today is " + ChatColor.GREEN + now.toString(dtf));
 	}
